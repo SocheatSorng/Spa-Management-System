191bc52874cad046ff2173f14eca5b5e6c19e79e
Socheat <songsocheat911@gmail.com>
2025-03-20 20:53:35 +0700

    Dashboard UI

--- Dashboard.cs
+++ Dashboard.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace Spa_Management_System
 {
-    public partial class Dashboard: Form
+    public partial class Dashboard : Form
     {
         public Dashboard()
         {
             InitializeComponent();
+        }
+
+        private void bunifuIconButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void bunifuLabel28_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
